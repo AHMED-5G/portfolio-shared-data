65043331fa7befa76a37576732a76dc385a81964
ahmed-5g <ahmed_5g@yahoo.com>
2023-08-27 07:58:22 +0300

    fix: apiurls init

---
+++ constants/apiUrls.ts
@@ -0,0 +1,6 @@
+export const BASE_URL: string =
+  process.env.NODE_ENV === "development"
+    ? "http://127.0.0.1:3000"
+    : "https://api.example.com";
+export const USERS_URL: string = `${BASE_URL}/users`;
+export const REGISTER_URL: string = `${BASE_URL}/register`;
