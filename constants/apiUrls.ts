e29c8100a25bc631da0899fd3fe4f820d0d2764f
ahmed-5g <ahmed_5g@yahoo.com>
2023-09-05 07:53:05 +0300

    chore: by back end apiversion

--- constants/apiUrls.ts
+++ constants/apiUrls.ts
@@ -1,5 +1,7 @@
-export const USERS_PATH: string = `/users`;
-export const REGISTER_PATH: string = `/users/register`;
-export const LOGIN_PATH: string = `/users/login`;
-export const REQUEST_RESET_PASSWORD_PATH: string = `/users/request-reset-password`;
-export const RESET_PASSWORD_PATH: string = `/users/reset-password`;
+export const API_PATH: string = "api/v1";
+export const USERS_PATH: string = API_PATH + `/users`;
+export const REGISTER_PATH: string = API_PATH + `/users/register`;
+export const LOGIN_PATH: string = API_PATH + `/users/login`;
+export const REQUEST_RESET_PASSWORD_PATH: string =
+  API_PATH + `/users/request-reset-password`;
+export const RESET_PASSWORD_PATH: string = API_PATH + `/users/reset-password`;
