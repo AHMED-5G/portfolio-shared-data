fa040befac5018af5a9917d68c627880fdcaf7ac
ahmed-5g <ahmed_5g@yahoo.com>
2023-09-07 01:30:47 +0300

    chore: by front end

--- constants/apiUrls.ts
+++ constants/apiUrls.ts
@@ -1,7 +1,7 @@
-export const API_PATH: string = "api/v1";
-export const USERS_PATH: string = API_PATH + `/users`;
-export const REGISTER_PATH: string = API_PATH + `/users/register`;
-export const LOGIN_PATH: string = API_PATH + `/users/login`;
-export const REQUEST_RESET_PASSWORD_PATH: string =
+export const API_PATH = "/api/v1";
+export const USERS_PATH = API_PATH + `/users`;
+export const REGISTER_PATH = API_PATH + `/users/register`;
+export const LOGIN_PATH = API_PATH + `/users/login`;
+export const REQUEST_RESET_PASSWORD_PATH =
   API_PATH + `/users/request-reset-password`;
-export const RESET_PASSWORD_PATH: string = API_PATH + `/users/reset-password`;
+export const RESET_PASSWORD_PATH = API_PATH + `/users/reset-password`;
